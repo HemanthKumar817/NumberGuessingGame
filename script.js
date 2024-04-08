7450f46348cf0f22a3a9c67df34a289069700f97
HemanthKumar817 <165409486+HemanthKumar817@users.noreply.github.com>
2024-04-08 22:40:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let score = 20;
+let highScore = 20;
+let guessCount = 0;
+
+checkButton.addEventListener('click', checkGuess);
+
+function checkGuess() {
+  const userGuess = Number(inputNumber.value);
+
+  if (guessCount === 1) {
+    guesses.textContent = "Previous guesses:";
+  }
+  guesses.textContent = `${guesses.textContent} ${userGuess}`;
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = "Congratulations! You got it right!";
+    lastResult.style.backgroundColor = "green";
+    lowOrHi.textContent = "";
+    setGameOver();
+  } else if (guessCount === 9) {
+    lastResult.textContent = "!!!GAME OVER!!!";
+    lowOrHi.textContent = "";
+    setGameOver();
+  } else {
+    lastResult.textContent = "Wrong!";
+    lastResult.style.backgroundColor = "red";
+    if (userGuess < randomNumber) {
+      lowOrHi.textContent = "Last guess was too low!";
+    } else if (userGuess > randomNumber) {
+      lowOrHi.textContent = "Last guess was too high!";
+    }
+  }
+
+  guessCount++;
+  inputNumber.value = "";
+  inputNumber.focus();
+}
+
+// ... rest of the code ...
